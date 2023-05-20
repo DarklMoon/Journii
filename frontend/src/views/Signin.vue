49f1f34ad523441b2fbcea28196fb943cf6707ae
Rerksurat Attinantachai <64070094@it.kmitl.ac.th>
2023-05-20 11:26:54 +0700

    edit signin&up

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -5,20 +5,20 @@
             <div class="form-value p-10 text-white">
                 <form action="">
                     <h2 class="text-center text-4xl">Log in</h2>
-                    <div class="inputbox relative mx-30 w-310 py-3">
+                    <div class="inputbox relative mx-30 w-310 pt-6 pb-1">
                         <ion-icon class="absolute right-0 " name="mail-outline"></ion-icon>
                         <input class="w-full h-50 bg-transparent" type="email" placeholder="Email" v-model='get_in.email'
                             required>
                     </div>
-                    <div class="inputbox relative mx-30 w-310 py-3">
+                    <div class="inputbox relative mx-30 w-310 pt-6 pb-1">
                         <ion-icon class="absolute right-0" name="lock-closed-outline"></ion-icon>
                         <input class="w-full h-50 bg-transparent" type="password" placeholder="Password"
                             v-model='get_in.pass' required>
                     </div>
-                    <button type="button" class="mt-5 mb-2 rounded-2xl w-full h-8 text-black bg-white" @click="Login()">Log
+                    <button type="button" class="mt-5 mb-2 rounded-2xl w-full h-8 text-black bg-white transition duration-100 ease-in-out hover:bg-neutral-300" @click="Login()">Log
                         in</button>
                     <div class="register">
-                        <p>Don't have an account? <router-link :to="{ name: 'signup' }">Register</router-link>
+                        <p>Don't have an account? <router-link :to="{ name: 'signup' }" class="hover:text-neutral-300">Sign Up</router-link>
                         </p>
                     </div>
                 </form>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="back flex justify-center items-center w-full h-full relative">
         <div
-            class="form-box flex backdrop-blur justify-center items-center relative w-400 h-450 bg-transparent border-2 border-neutral-300 rounded-2xl">
+            class="form-box flex backdrop-blur justify-center items-center relative h-450 bg-transparent border-2 border-neutral-300 rounded-2xl">
             <div class="form-value p-10 text-white">
                 <form action="">
-                    <h2 class="text-center text-4xl">Register</h2>
+                    <h2 class="text-center text-4xl">Sign Up</h2>
                     <div class="flex relative w-1/2 pt-6 ">
                         <input class="h-50 bg-transparent inputbox pb-1 pr-6 mr-10" type="text" v-model="new_user.firstname"
                             placeholder="Firstname" required>
@@ -28,7 +28,12 @@
                         <input class="w-full h-50 bg-transparent" type="password" v-model="new_user.password"
                             placeholder="Password" required>
                     </div>
-                    <div class="gender pt-6">
+                    <div class="inputbox relative mx-30 w-310 pt-6 pb-1">
+                        <ion-icon class="absolute right-0" name="lock-closed-outline"></ion-icon>
+                        <input class="w-full h-50 bg-transparent" type="password" v-model="new_user.confirm_password"
+                            placeholder="Confirm Password" required>
+                    </div>
+                    <div class="gender pt-6 ">
                         <label for="" class="">Gender</label><br>
                         <input class="checked:bg-gray-300" type="radio" value="female" v-model="new_user.gender" required>
                         <label for="" class="pl-1 pr-5">Female</label>
@@ -38,10 +43,9 @@
                     <!-- <div class="forget text-center">
                             <a href="#">Forget Password</a>
                         </div> -->
-                    <button type="submit" class="mt-5 mb-2 rounded-2xl w-full h-8 text-black bg-white" @click="Login()">Log
-                        in</button>
-                    <div class="register">
-                        <p>Already have an account? <router-link :to="{ name: 'signin' }">Log in</router-link></p>
+                    <button type="submit" class="mt-5 mb-2 rounded-2xl w-full h-8 text-black bg-white transition duration-100 ease-in-out hover:bg-neutral-300" @click="signup()">Sign Up</button>
+                    <div class="register text-center">
+                        <p>Already have an account? <router-link :to="{ name: 'signin' }" class="hover:text-neutral-300">Log in</router-link></p>
                     </div>
                 </form>
 
@@ -52,6 +56,7 @@
 </template>
 
 <script>
+// import axios from './plugins/axios.js'
 export default {
     name: 'Signup',
     data() {
@@ -62,18 +67,33 @@ export default {
                 username: '',
                 email: '',
                 password: '',
+                confirm_password: '',
                 gender: '',
             }
         }
     },
     methods: {
-        GoSignin() {
-            window.location.href = "SignIn.html";
-        },
-        Login() {
-            let arr_user = JSON.parse(localStorage.getItem('User'))
-            arr_user.push(this.new_user)
-            localStorage.setItem('User', JSON.stringify(arr_user))
+        signup() {
+            // let arr_user = JSON.parse(localStorage.getItem('User'))
+            // arr_user.push(this.new_user)
+            // localStorage.setItem('User', JSON.stringify(arr_user))
+            let data = {
+                username: this.username,
+                password: this.password,
+                confirm_password: this.confirm_password,
+                email: this.email,
+                gender: this.mobile,
+                first_name: this.first_name,
+                last_name: this.last_name,
+            }
+            axios
+                .post("/user/signup", data)
+                .then((res) => {
+                    alert("Sign up Success");
+                })
+                .catch((err) => {
+                    alert(err.response.data.details.message)
+                });
         }
     }
 }
