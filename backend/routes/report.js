984344c665412cb12bc6464433b6dbd4051abc83
Rerksurat Attinantachai <64070094@it.kmitl.ac.th>
2023-05-22 01:09:21 +0700

    update report

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -5,35 +5,49 @@ const { isLoggedIn } = require("../middlewares");
 router = express.Router();
 
 //Create Reports
-router.post("/reports", async function (req, res, next){
-    try {
+router.post("/reports", async function (req, res, next) {
 
-      return res.status(201).json({
-        msg: "Create report!",
-      });
-    } catch (error) {
-      return res.status(400).json(err);
-    }
+  report_type = req.body.selected
+  description = req.body.description
+  const conn = await pool.getConnection()
+  await conn.beginTransaction()
+
+  try {
+    let results = await conn.query(
+      "INSERT INTO report(report_type, report_info) " +
+      "VALUES(?, ?);",
+      [report_type, description]
+    );
+    await conn.commit();
+    return res.status(201).json({
+      msg: "Create report!",
+    });
+  } catch (error) {
+    await conn.rollback();
+    return res.status(400).json(err);
+  } finally {
+    conn.release();
+  }
 })
 
 //Get Report detail
 router.get("/reports/:id", function (req, res, next) {
 
-    return res.status(200).json({
-        msg: `Get Report ${req.params.id}!`
-    })
+  return res.status(200).json({
+    msg: `Get Report ${req.params.id}!`
+  })
 })
 
 //Delete Report
 router.delete("/reports/:id", async function (req, res, next) {
-    try {
+  try {
 
-      return res.status(200).json({
-        msg: `Delete report ${req.params.id}!`,
-      });
-    } catch (error) {
-      return res.status(400).json(err);
-    }
+    return res.status(200).json({
+      msg: `Delete report ${req.params.id}!`,
+    });
+  } catch (error) {
+    return res.status(400).json(err);
+  }
 })
 
 exports.router = router;
--- frontend/src/views/MemoReport.vue
+++ frontend/src/views/MemoReport.vue
@@ -17,21 +17,33 @@
             <div class="w-4/5 h-screen p-5 pt-10">
                 <div class="flex flex-col justify-center items-center pt-3">
                     <div class='relative w-[50vw] border-2 border-[#513F3F] rounded-md p-4 bg-white'>
-                        <select name="" id="" class="input-memoAdd w-full mb-5">
-                            <option disabled selected value="">Type of Report</option>
-                            <option value="">Spam</option>
-                            <option value="">Fake Information</option>
-                        </select><br>
+                        <div class=" mb-5">
+                            <select name="" id="" class="input-memoAdd w-full" v-model="v$.selected.$model"
+                                :class="{ 'border-b-2 border-rose-600': v$.selected.$error }">
+                                <option disabled selected value="">Type of Report</option>
+                                <option value="Spam">Spam</option>
+                                <option value="Fake Information">Fake Information</option>
+                                <option value="Abusive Content">Abusive Content</option>
+                                <option value="Other">Other</option>
+                            </select><br>
+                            <template v-if="v$.selected.$error">
+                                <p class="text-rose-600" v-for="error of v$.selected.$errors" :key="error.$uid">
+                                    {{ error.$message }}
+                                </p>
+                            </template>
+                        </div>
+
                         <h1 class="text-xl" for="">Description</h1>
-                        <textarea name="" id="" class="block mt-1 p-2 text-lg border-2 border-gray-400 rounded-md w-full"
-                            rows="8"></textarea>
+                        <textarea name="" id="" v-model="description"
+                            class="block mt-1 p-2 text-lg border-2 border-gray-400 rounded-md w-full" rows="8"></textarea>
                         <div class="text-center mt-5">
-                            <button @click="$router.go(-1)" class=" text-[1.1vw] h-3/4 p-2 px-3 bg-green rounded-md hover:text-white mr-6">
+                            <button @click="$router.go(-1)"
+                                class=" text-[1.1vw] h-3/4 p-2 px-3 bg-green rounded-md hover:text-white mr-6">
                                 Cancel
                             </button>
-                            <button class=" text-[1.1vw] h-3/4 p-2 px-3 bg-green rounded-md hover:text-white">
-                                <router-link :to="{ name: 'report' }">Submit</router-link>
-                            </button>
+                            <button @click="submit()"
+                                class="text-[1.1vw] h-3/4 p-2 px-3 bg-green rounded-md hover:text-white">
+                                Submit </button>
                         </div>
 
                     </div>
@@ -44,30 +56,48 @@
 
 <script>
 import axios from '@/plugins/axios.js'
+import useVuelidate from "@vuelidate/core";
+import { required, email, minLength, maxLength, sameAs, helpers } from '@vuelidate/validators'
+
 export default {
-    name: 'list',
+    name: 'report',
+    setup() {
+        return { v$: useVuelidate() };
+    },
     data() {
         return {
-            getData: [],
-            blog: {}
+            selected: '',
+            description: '',
         }
     },
-    mounted() {
-        this.getBlogDetail(this.$route.params.id);
+    validations() {
+        return {
+            selected: {
+                required
+            }
+        }
     },
     methods: {
-        getBlogDetail(blogId) {
-            axios
-                .get(`/memoes/${blogId}`)
-                .then((response) => {
-                    // console.log(response.data)
-                    this.blog = response.data;
-                    // console.log(this.blog)
-                })
-                .catch((error) => {
-                    this.error = error.response.data.message;
-                });
-        },
+        submit() {
+            this.v$.$touch();
+            if (!this.v$.$invalid) {
+                let data = {
+                    selected: this.selected,
+                    description: this.description,
+                }
+                axios
+                    .post("/reports", data)
+                    .then((res) => {
+                        alert("Report Success");
+                        this.$router.push({
+                            name: 'list'
+                        })
+                    })
+                    .catch((err) => {
+                        alert(err)
+                    });
+            }
+        }
     },
 
 }
